21bcdc03a1b2236e2e0b61f92c3a9a18ec19cd37
Niklas Wagner <niklas.wagner@finabro.com>
2023-09-18 19:54:41 +0200

    First Commit (Niklas)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!\n");
-        System.out.printf("Hello and welcome!\n");
+        System.out.printf("Niklas is also here");
 
         // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
