1f115a96e8646afb9424b5ce58f3f96fb682d9e7
Bernahrd Erös <eroes.bernhard@gmail.com>
2023-10-02 17:08:00 +0200

    feat: game start fix: fixed color selection error on on lower case letters

--- src/main/java/VierGewinnt/VierGewinnt.java
+++ src/main/java/VierGewinnt/VierGewinnt.java
@@ -6,21 +6,40 @@ import java.util.Scanner;
 public class VierGewinnt {
     public static void main(String[] args) {
 
+        //instructions how to play the game
         System.out.print("Spielbeschreibung: tbd \nSpielanleitung: tbd \n");
 
-        // user input - color selection
+        // game start
         Scanner scanner = new Scanner(System.in);
+        char start;
+
+        do{
+            System.out.print("please start the game with S...\n");
+            start = scanner.next().charAt(0);
+            if (start != 'S' && start != 's'){
+                System.out.print("INVALID INPUT, start the game with S...\n");
+            }
+        }
+        while(start != 'S' && start != 's');
+        System.out.print("game started...\n");
+
+        // user input - color selection
+        //Scanner scanner = new Scanner(System.in);
         char p1Color;
         char p2Color;
 
         do{
             System.out.print("player 1 please pick your color:\nR for red, G for green, B for blue...\n");
             p1Color = scanner.next().charAt(0);
-            if (p1Color != 'R' && p1Color != 'G' && p1Color != 'B'){
+            if (p1Color != 'R' && p1Color != 'r' &&
+                p1Color != 'G' && p1Color != 'g' &&
+                p1Color != 'B' && p1Color != 'b'){
                 System.out.print("INVALID INPUT, please choose between R, G or B...\n");
             }
         }
-        while(p1Color != 'R' && p1Color != 'G' && p1Color != 'B');
+        while(p1Color != 'R' && p1Color != 'r' &&
+              p1Color != 'G' && p1Color != 'g' &&
+              p1Color != 'B' && p1Color != 'b');
         System.out.print("player 1 color: " + p1Color + "\n");
 
         do{
@@ -29,11 +48,16 @@ public class VierGewinnt {
             if (p2Color == p1Color){
                 System.out.print("INVALID INPUT, please use another color than player 1...\n");
             }
-            if (p2Color != 'R' && p2Color != 'G' && p2Color != 'B'){
+            if (p2Color != 'R' && p2Color != 'r' &&
+                p2Color != 'G' && p2Color != 'g' &&
+                p2Color != 'B' && p2Color != 'b'){
                 System.out.print("INVALID INPUT, please choose between R, G or B...\n");
             }
         }
-        while(p2Color != 'R' && p2Color != 'G' && p2Color != 'B' || p2Color == p1Color);
+        while(p2Color != 'R' && p2Color != 'r' &&
+              p2Color != 'G' && p2Color != 'g' &&
+              p2Color != 'B' && p2Color != 'b' ||
+              p2Color == p1Color);
         System.out.print("player 2 color: " + p2Color + "\n");
 
 
