6a95f7e621adc945f91d94a83a1177d8e9212eb2
Niklas Wagner <niklas.wagner@finabro.com>
2023-11-24 18:49:55 +0100

    SEG-24: resolved merge conflicts

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,45 @@
+import connect4.Board;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    Board testBoard;
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+        testBoard = new Board();
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void displayBoardEmptyTest() {
+
+        // Auslagern in ein Textfile
+        String expectedBoard = "\n" +
+                "Current Board: \n" +
+                "\n" +
+                "|  1  |  2  |  3  |  4  |  5  |  6  |  7  |\n" +
+                "|-----------------------------------------|\n" +
+                "|     |     |     |     |     |     |     |\n" +
+                "|-----------------------------------------|\n" +
+                "|     |     |     |     |     |     |     |\n" +
+                "|-----------------------------------------|\n" +
+                "|     |     |     |     |     |     |     |\n" +
+                "|-----------------------------------------|\n" +
+                "|     |     |     |     |     |     |     |\n" +
+                "|-----------------------------------------|\n" +
+                "|     |     |     |     |     |     |     |\n" +
+                "|-----------------------------------------|\n" +
+                "|     |     |     |     |     |     |     |\n" +
+                "|-----------------------------------------|\n";
+
+        String actualBoard = testBoard.generateBoard();
+        assertEquals(expectedBoard, actualBoard);
+
+    }
+}
